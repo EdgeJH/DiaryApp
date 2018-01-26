c0f30847821af83ab193c67a9b64ae1bf51020bf
edgejh <edgejh@feelteller.com>
2018-01-26 15:36:22 +0900

    Initial commit

---
+++ app/src/main/java/com/edge/diaryapp/MainActivity.java
@@ -0,0 +1,29 @@
+package com.edge.diaryapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button = findViewById(R.id.button);
+        button.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.button:
+                Intent intent = new Intent(MainActivity.this,Main2Activity.class);
+                startActivity(intent);
+                break;
+        }
+    }
+}
